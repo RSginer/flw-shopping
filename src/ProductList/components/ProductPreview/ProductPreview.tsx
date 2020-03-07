60fb4ab6bb7183bdbd426e9315d426fff473f794
Rubén Soler <ruben.soler@exp360.com>
2020-03-07 18:57:29 +0100

    Style ProductPreview

--- src/ProductList/components/ProductPreview/ProductPreview.tsx
+++ src/ProductList/components/ProductPreview/ProductPreview.tsx
@@ -6,7 +6,7 @@ function ProductPreview(props: Product) {
   return (
     <div className="product-preview col-xs-6 col-sm-3 col-md-2">
       <div className="product-preview-content">
-        <div className="product-image" style={{ backgroundImage: `url(${props.image_url}` }}>
+        <div className="product-image" style={{ backgroundImage: `url('${props.image_url}'` }}>
         </div>
         <div className="product-body">
           <span className="product-name">{props.productName}</span>
