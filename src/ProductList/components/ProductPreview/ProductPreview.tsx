d29a3fabbedd08c6000fe239673d8572508fc07c
Rubén Soler <ruben.soler@exp360.com>
2020-03-07 21:01:04 +0100

    Feature favorite icons

--- src/ProductList/components/ProductPreview/ProductPreview.tsx
+++ src/ProductList/components/ProductPreview/ProductPreview.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './ProductPreview.scss';
 import { Product } from '../../../models/Product';
+import FavoriteHeart from './components/FavoriteHeart/FavoriteHeart';
 
 function ProductPreview(props: Product) {
   return (
     <div className="product-preview col-xs-6 col-sm-3 col-md-2">
       <div className="product-preview-content">
         <div className="product-image" style={{ backgroundImage: `url('${props.image_url}'` }}>
+          <FavoriteHeart isFavorite={props.favorite !== 0} />
         </div>
         <div className="product-body">
           <span className="product-name">{props.productName}</span>
