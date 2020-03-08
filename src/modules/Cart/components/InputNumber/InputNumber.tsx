317b7c9ac627c3e93236d4fd9c13a7df0eef5917
Rubén Soler <ruben.soler@exp360.com>
2020-03-08 21:06:51 +0100

    Style input number

--- src/modules/Cart/components/InputNumber/InputNumber.tsx
+++ src/modules/Cart/components/InputNumber/InputNumber.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { Plus, Minus } from 'react-feather';
 
 import './InputNumber.scss';
 
 function InputNumber(props: any) {
   return (
     <div className="input-number">
-
+      <Plus size={15} />
+      <input className="input" type="number" value="1" max="100" />
+      <Minus size={15} />
     </div>
   );
 }
