6169660371dcadfc56bf48423bb1d283d354e39e
Rubén Soler <ruben.soler@exp360.com>
2020-03-09 00:12:56 +0100

    Refactor move Header to module

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { history } from './createStore';
 import { Route } from "react-router-dom";
 import ProductContainer from './modules/Product/container';
 import CartContainer from './modules/Cart/container';
-import Header from './components/Header/Header';
+import Header from './modules/Header/components/Header';
 
 
 
