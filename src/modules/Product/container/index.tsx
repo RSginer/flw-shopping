05098407e63a1acbec83d3ab6b9eea1fc1711e80
Rubén Soler <ruben.soler@exp360.com>
2020-03-09 18:00:42 +0100

    Fix isFavorites param on fetch

--- src/modules/Product/container/index.tsx
+++ src/modules/Product/container/index.tsx
@@ -33,7 +33,7 @@ export function ProductContainer(props: IProductContainer = { isFavorites: false
 
 
     if (!fetched)
-      dispatch({ type: types.FETCH_PRODUCTS, payload: props.favorites })
+      dispatch({ type: types.FETCH_PRODUCTS, payload: props.isFavorites })
   }, [dispatch, fetched, props])
 
   return (
