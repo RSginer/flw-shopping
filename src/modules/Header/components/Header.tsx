cbc7b42cf8289599103ac1a2334eb53952898243
Rubén Soler <ruben.soler@exp360.com>
2020-03-09 01:01:34 +0100

    Feature navigate

--- src/modules/Header/components/Header.tsx
+++ src/modules/Header/components/Header.tsx
@@ -3,22 +3,29 @@ import React from 'react';
 import './Header.scss';
 import FavoriteHeart from '../../Product/components/FavoriteHeart/FavoriteHeart';
 import { ShoppingCart } from 'react-feather';
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
+
 
 export interface IHeader {
   title: string;
 }
 
 function Header(props: IHeader) {
+
+  const dispatch = useDispatch();
+
+
   return (
     <div className="header">
       <div className="container-fluid header-content">
         <div className="favorites">
-          <FavoriteHeart isFavorite={true}/>
+          <FavoriteHeart isFavorite={true} />
         </div>
         <h1 className="app-title">{props.title}</h1>
         <div className="cart-icon">
-          <span className="hidden-md hidden-lg">
-            <ShoppingCart size={20} />
+          <span className="hidden-md hidden-lg" onClick={() => dispatch(push('/cart'))}>
+              <ShoppingCart size={20} />
           </span>
         </div>
       </div>
