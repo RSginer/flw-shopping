38057c7f8ae2cea4aa4181ca5cfaa6408cfc2250
Rubén Soler <ruben.soler@exp360.com>
2020-03-09 13:45:33 +0100

    Fix ProductList key

--- src/modules/Product/components/ProductList/ProductList.tsx
+++ src/modules/Product/components/ProductList/ProductList.tsx
@@ -14,7 +14,7 @@ export function ProductList(props: IProductList) {
   return (
     <div className="product-list">
       <div className="list row col-xs-12">
-        {props.products.map((product: Product) => <ProductPreview onAddToCart={props.onAddToCart} key={product.id} product={product} />)}
+        {props.products.map((product: Product, i) => <ProductPreview onAddToCart={props.onAddToCart} key={i} product={product} />)}
       </div>
     </div>
   );
